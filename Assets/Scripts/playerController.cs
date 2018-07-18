0fd50e735b33a79bebaa0b9164b11c97e8948ae3
Coolbro009 <tylerchew5407@gmail.com>
2018-07-18 15:09:38 -0700

    Transfering project to Github.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+	public float speed = 4f;
+	public GameObject Laser;
+	Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		Shoot();
+	}
+
+	void Move() {
+		transform.position = pos;
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && pos.x < 6.6) {
+			pos.x += speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) && pos.x > -6.6) {
+			pos.x -= speed * Time.deltaTime;
+		}
+	}
+
+	void Shoot() {
+		if (Input.GetMouseButtonDown(0)) {
+			GameObject laser = Instantiate(Laser);
+			laser.transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+			
+		}
+	}
+}
